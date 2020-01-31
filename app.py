f392dd18db256e7877048e026a676ea0ea8694ea
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-31 15:40:52 +0000

    Pagination showing 6 limit - not yet passed in template

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import math
 import pymongo
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, pymongo
@@ -23,22 +22,13 @@ mongo = PyMongo(app)
 
 # Routes
 
-# Index(Home) Route
 @app.route('/')
 def index():
-    # Logic for pagination
-    page_limit = 6 
-    current_page = int(request.args.get('current_page', 1))
-    total = mongo.db.task.count()
-    pages = range(1, int(math.ceil(total / page_limit)) + 1)
-    tasks=mongo.db.tasks.find()..sort('_id', pymongo.ASCENDING).skip((current_page - 1)*page_limit).limit(page_limit)
-    return render_template('index.html',task=tasks, title='Home', current_page=current_page, pages=pages )
     
-# View Recipe Route
-
+    tasks=mongo.db.tasks.find().limit(6)
+    return render_template('index.html', tasks=tasks)
+    
 
 
 if __name__=='__main__':
     app.run(debug=True)
-    
-    
