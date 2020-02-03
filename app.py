26cd06c107a12609653e270e162d8770e871f612
pranitastudent <pranita_gandhi@hotmail.com>
2020-02-03 12:15:19 +0000

    Registration and login removed so search logic can be tested

--- app.py
+++ app.py
@@ -81,36 +81,14 @@ def about():
 
 # Login- Code adapted from Corey Schafer Flask Series
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            return redirect(url_for('index'))
-        flash('You are successfully logged in', 'success')
-        
-        else:
-            flash('Your password or username is incorrct. Please check', 'danger')
-            
+@app.route('/login')
+def login():               
     return render_template('login.html', title='Login', form=form)
 
 # Register- Code adapted from Corey Schafer Flask Series
 
-@users.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+@app.route("/register")
+def register():   
     return render_template('register.html', title='Register', form=form)
 
 
