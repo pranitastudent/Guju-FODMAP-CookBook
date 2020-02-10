c2400fdbac646d38e32aa92eab648711a409059f
pranitastudent <pranita_gandhi@hotmail.com>
2020-02-10 15:14:13 +0000

    Filter function built for courses and works

--- app.py
+++ app.py
@@ -77,22 +77,25 @@ def findtask():
 
 # Filter for course only
 
-@app.route('/filtercourses')
+@app.route('/filtercourses', methods=['GET', 'POST'])
 
 def filtercourses():
-    query = request.args.get('query')
     
-    results = mongo.db.tasks.find({"recipe_course" : {"$regex": "query", "$options": "i"}})
+    if request.method == "POST":
+        for i in request.form:
+            if i == "recipe_course":
+                filter_items = []
+                items = request.form.getlist("recipe_course")
+                my_key = request.form
+                for item in items:
+                    for key in my_key:
+                        filter_items.append({key:item})
+                        results = mongo.db.tasks.find({'$and': [{'$or': filter_items}]})
  
-    if results.count():
-        return render_template('filter.html', results=results, query=query, title="Filter")        
-    else:
-        flash('No results were found', 'info') 
+        print(results.count())
+           
+    return render_template('filter.html', results=results, title="Filter")        
         
-    return render_template('index.html')
-
-
-    
    
 
 # Create Recipe
@@ -103,6 +106,8 @@ def filtercourses():
     
 
 # Up votes
+
+
 
 
     
