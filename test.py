fd476d634eb0915d315de848543c6f4fbc9c450a
pranitastudent <pranita_gandhi@hotmail.com>
2020-02-14 15:25:50 +0000

    Unit tests set up checking name of recipe in db and description - both passed

---
+++ test.py
@@ -0,0 +1,30 @@
+from app import app
+import unittest
+from flask import Flask, session, url_for
+
+# Check for recipe in database test
+
+class FlaskTestCase(unittest.TestCase):
+    """ Test to check recipe description in database (Undhiyu)  """
+    def test_task(self):
+        # Test a particular recipe description
+        tester = app.test_client(self)
+        response = tester.get('/task/{}'.format("5e33ff3c1c9d440000bcfe4d"), content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"Take 1 cup gram flour" in response.data)
+    
+    """ Test to check recipe name in db"""    
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"Undhiyu" in response.data)
+        
+# Search Test
+
+def test_        
+        
+            
+
+
+
