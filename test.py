68bf36bd81c75ab9a105cc2520b9df038de871d7
pranitastudent <pranita_gandhi@hotmail.com>
2020-02-14 15:51:23 +0000

    Login, Register and Logout functions unittested- all passed

--- test.py
+++ test.py
@@ -5,6 +5,31 @@ from flask import Flask, session, url_for
 # Check for recipe in database test
 
 class FlaskTestCase(unittest.TestCase):
+    
+    """ Test Register function"""
+    
+    def test_register(self):
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"Username" in response.data)
+        
+    """ Test Login function"""
+    
+    def test_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"Password" in response.data) 
+        
+    """ Test Logout function"""
+    
+    def test_logout(self):
+        tester = app.test_client(self)
+        response = tester.get('/logout', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+    
+     
     """ Test to check recipe description in database (Undhiyu)  """
     def test_task(self):
         # Test a particular recipe description
@@ -22,6 +47,7 @@ class FlaskTestCase(unittest.TestCase):
         
 # Search Test function
 
+    """ Test to check search function"""    
     def test_findtask(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
@@ -30,21 +56,36 @@ class FlaskTestCase(unittest.TestCase):
         
 # Filters - Course and allergens
 
+    """ Test to check filtering of courses"""    
     def test_filtercourses(self):
         tester = app.test_client(self)
         response = tester.post('/filtercourses', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b"Main")
+        self.assertTrue(b"Main" in response.data)
         
+        
+    """ Test to filtering of allergens"""        
     def test_filterallergens(self):
         tester = app.test_client(self)
         response = tester.post('/filterallergens', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b"Nuts")
-            
         
-                
-                
+# Add a recipe 
+     
+    """ Test addition of recipe"""        
+    def test_create_task(self):
+        tester = app.test_client(self)
+        response = tester.post('/create_task', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(b"Recipe Name")  
+        
+    """ Test editing a recipe"""
+    def test_update_task(self):
+        tester = app.test_client(self)
+        response = tester.post('/create_task', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(b"Cooking Time")           
         
             
 
