0a9635ae1bb13006e8f6ad9157ad299ade6833cd
pranitastudent <pranita_gandhi@hotmail.com>
2020-02-14 15:05:30 +0000

    Back to the top button added, fade out for alerts and year function(automatically changes with each progressing year)

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+// Year-  Code Taken from Brad Traversy's Course Python Django to Dev (Nov 2018)
+const date = new Date();
+document.querySelector('.year').innerHTML = date.getFullYear();
+
+// End of Code taken
+
+// Fade Out JS for Alerts
+
+setTimeout(function(){
+    $('#message').fadeOut('fast');
+}, 2000);
+
+// Back To The Top Button- adapted from  Traversy Media you tube video project :'myTunes Site'
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 850) {
+            $('#topBtn').fadeIn();
+
+        }
+        else {
+            $("#topBtn").fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#topBtn').click(function() {
+        $('body,html').animate({
+                scrollTop: 0
+            },
+            200
+        );
+        return false;
+    });
+});
